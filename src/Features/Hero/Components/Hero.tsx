62f32881e0f0581ff6f92f435968adcb42eb61aa
moshaosama <mohamedOSFekry@gmail.com>
2025-08-06 23:07:06 +0300

    feat: update Hero component content or structure

--- src/Features/Hero/Components/Hero.tsx
+++ src/Features/Hero/Components/Hero.tsx
@@ -1,16 +1,23 @@
 import { cn } from "../../../lib/utils";
 import { AspectRatio } from "../../../components/ui/aspect-ratio";
 import ButtonSection from "../../../components/Shared/ButtonSection";
+import useAnimationHero from "../Hooks/useAnimation";
 
 const Hero = () => {
+  const { isAnimateText, isAnimateImages } = useAnimationHero();
   return (
     <>
       <AspectRatio ratio={4 / 0} className="relative">
         <img src="/1.jpg" alt="HeroImage.png" className="object-fill" />
       </AspectRatio>
       <div className="absolute top-0 w-full h-[121vh] bg-black/50"></div>
-      <div className="absolute top-0 text-white">
-        <div className="my-56 mx-28 flex flex-col gap-14">
+      <div
+        className={cn(
+          "absolute transition-all duration-700 text-white",
+          isAnimateText ? "top-0" : "top-72"
+        )}
+      >
+        <div className={cn("my-56 mx-28 flex flex-col gap-14")}>
           <h1 className="text-9xl font-bold w-52">
             Unveiling Architectural Mastery
           </h1>
@@ -21,12 +28,17 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className={cn("absolute top-[55pc] flex gap-5 left-28")}>
+      <div
+        className={cn(
+          "absolute top-[55pc] transition-all duration-700 flex gap-5 left-28",
+          isAnimateImages ? "top-[55pc] " : "top-[63pc]"
+        )}
+      >
         <div className="overflow-hidden group hover:rounded-2xl not-hover:rounded-2xl">
           <img
             src="/1.jpg"
             alt="/2.jpg"
-            className="w-40 h-40 object-cover rounded-2xl group-hover:scale-150 group-hover:rounded-2xl transition-all duration-300 group-hover:rotate-6"
+            className="w-40 cursor-pointer h-40 object-cover rounded-2xl group-hover:scale-150 group-hover:rounded-2xl transition-all duration-300 group-hover:rotate-6"
           />
         </div>
 
@@ -34,7 +46,7 @@ const Hero = () => {
           <img
             src="/3.jpg"
             alt="HeroImage.png"
-            className="w-40 h-40 object-cover rounded-2xl group-hover:scale-150 group-hover:rounded-2xl transition-all duration-300 group-hover:-rotate-6"
+            className="w-40 h-40 cursor-pointer object-cover rounded-2xl group-hover:scale-150 group-hover:rounded-2xl transition-all duration-300 group-hover:-rotate-6"
           />
         </div>
       </div>
