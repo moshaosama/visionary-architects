fae0a6df410a52c2b8b355c1940d130679d3892a
moshaosama <mohamedOSFekry@gmail.com>
2025-08-06 21:58:08 +0300

    feat: add Layouts structure

---
+++ src/Layouts/Navbar.tsx
@@ -0,0 +1,36 @@
+import { Button } from "../components/ui/button";
+import Links_Render from "../components/ui/links";
+import { cn } from "../lib/utils";
+import Container from "../Styles/GlobalStyle";
+import { AspectRatio } from "@radix-ui/react-aspect-ratio";
+
+const Navbar = () => {
+  return (
+    <>
+      <AspectRatio
+        ratio={14 / 1}
+        className={cn("w-full flex items-center justify-between bg-[#242424]")}
+      >
+        <div
+          className={cn(Container, "flex w-full justify-between items-center")}
+        >
+          <div>
+            <img
+              src="/logo.png"
+              alt="Photo by Drew Beamer"
+              className="rounded-lg object-cover w-24"
+            />
+          </div>
+
+          <Links_Render />
+
+          <div>
+            <Button className="border-1 border-white">Let's talk</Button>
+          </div>
+        </div>
+      </AspectRatio>
+    </>
+  );
+};
+
+export default Navbar;
