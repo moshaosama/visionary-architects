96aab29b8ece60e2264fd9e5726b12f7fbb3925c
moshaosama <mohamedOSFekry@gmail.com>
2025-08-07 15:34:31 +0300

    feat: enhance Hero component structure

--- src/Features/Hero/Components/Hero.tsx
+++ src/Features/Hero/Components/Hero.tsx
@@ -7,53 +7,62 @@ const Hero = () => {
   const { isAnimateText, isAnimateImages } = useAnimationHero();
   return (
     <>
-      <AspectRatio ratio={4 / 0} className="relative">
-        <img src="/1.jpg" alt="HeroImage.png" className="object-fill" />
-      </AspectRatio>
-      <div className="absolute top-0 w-full h-[121vh] bg-black/50"></div>
-      <div
-        className={cn(
-          "absolute transition-all duration-700 text-white",
-          isAnimateText ? "top-0" : "top-72"
-        )}
-      >
-        <div className={cn("my-56 mx-28 flex flex-col gap-14")}>
-          <h1 className="text-9xl font-bold w-52">
-            Unveiling Architectural Mastery
-          </h1>
-          <p className="text-[gray] w-[30pc] text-xl">
-            Our architects breathe life into dreams, creating environments where
-            innovation meets tradition, and spaces transcend mere structures
-          </p>
-        </div>
-      </div>
-
-      <div
-        className={cn(
-          "absolute top-[55pc] transition-all duration-700 flex gap-5 left-28",
-          isAnimateImages ? "top-[55pc] " : "top-[63pc]"
-        )}
-      >
-        <div className="overflow-hidden group hover:rounded-2xl not-hover:rounded-2xl">
+      <div className={cn("relative")}>
+        <AspectRatio ratio={7 / 4.44}>
           <img
             src="/1.jpg"
-            alt="/2.jpg"
-            className="w-40 cursor-pointer
-             h-40 object-cover rounded-2xl group-hover:scale-150 group-hover:rounded-2xl transition-all duration-300 group-hover:rotate-6"
+            alt="HeroImage.png"
+            className="object-fill h-[75pc] w-full"
           />
+        </AspectRatio>
+        <div className="absolute top-0 w-full h-[121vh] bg-black/50"></div>
+        <div
+          className={cn(
+            "absolute transition-all duration-700 left-28 text-white",
+            isAnimateText ? "top-0" : "top-72"
+          )}
+        >
+          <div className={cn("my-56 mx-28 flex flex-col gap-14")}>
+            <h1 className="text-9xl font-bold w-52">
+              Unveiling Architectural Mastery
+            </h1>
+            <p className="text-[gray] w-[30pc] text-xl">
+              Our architects breathe life into dreams, creating environments
+              where innovation meets tradition, and spaces transcend mere
+              structures
+            </p>
+          </div>
         </div>
 
-        <div className="overflow-hidden group hover:rounded-2xl not-hover:rounded-2xl">
-          <img
-            src="/3.jpg"
-            alt="HeroImage.png"
-            className="w-40 h-40 cursor-pointer
+        <div
+          className={cn(
+            "flex flex-col gap-5 absolute top-[55pc] left-56 transition-all duration-700 ",
+            isAnimateImages ? "top-[55pc] " : "top-[63pc]"
+          )}
+        >
+          <div className={cn("flex gap-5")}>
+            <div className="overflow-hidden group hover:rounded-2xl not-hover:rounded-2xl">
+              <img
+                src="/1.jpg"
+                alt="/2.jpg"
+                className="w-40 cursor-pointer
+             h-40 object-cover rounded-2xl group-hover:scale-150 group-hover:rounded-2xl transition-all duration-300 group-hover:rotate-6"
+              />
+            </div>
+
+            <div className="overflow-hidden group hover:rounded-2xl not-hover:rounded-2xl">
+              <img
+                src="/3.jpg"
+                alt="HeroImage.png"
+                className="w-40 h-40 cursor-pointer
              object-cover rounded-2xl group-hover:scale-150 group-hover:rounded-2xl transition-all duration-300 group-hover:-rotate-6"
-          />
+              />
+            </div>
+          </div>
+
+          <ButtonSection Title="View Projects" />
         </div>
       </div>
-
-      <ButtonSection />
     </>
   );
 };
